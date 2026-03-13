48f5fed6125989e3ebe5f2ccf6f4761a8a28f633
Kallst <santiago.castrom@autonoma.edu.co>
2026-03-13 15:15:23 -0500

    Se configuraron todos los imports que necesitarán los Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
 {
