0cb258c2651363bee9135774ca7bda208d43ff77
Kallst <santiago.castrom@autonoma.edu.co>
2026-03-13 15:23:26 -0500

    Se configuraron todos los imports que necesitará el archivo api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\ApuestaController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
