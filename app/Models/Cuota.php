d91dc2ebe0e117c6c0f1a8ca5d4e3f2a0611b4e9
Kallst <santiago.castrom@autonoma.edu.co>
2026-03-15 12:01:03 -0500

    Se configuró el model Cuota para tener el modelo de inserción de la BD

--- app/Models/Cuota.php
+++ app/Models/Cuota.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cuota extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'evento_id',
+        'tipo_apuesta',
+        'cuota'
+    ];
+
+    public function evento()
+    {
+        return $this->belongsTo(Evento::class);
+    }
 }
