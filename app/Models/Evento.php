4a29c5fcfe14a25d165268343b3c5e3af56f8656
Kallst <santiago.castrom@autonoma.edu.co>
2026-03-15 12:01:38 -0500

    Se configuró el model Evento para tener el modelo de inserción de la BD

--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -7,5 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Evento extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'deporte',
+        'equipo_local',
+        'equipo_visitante',
+        'fecha',
+        'estado'
+    ];
+
+    public function cuotas()
+    {
+        return $this->hasMany(Cuota::class);
+    }
+
+    public function apuestas()
+    {
+        return $this->hasMany(Apuesta::class);
+    }
 }
