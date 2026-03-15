67d53eed69f05b91fa2e4102ae35c93b5556239e
Kallst <santiago.castrom@autonoma.edu.co>
2026-03-15 11:24:12 -0500

    Se configuró el EventoController con los respectivos métodos para consultar los eventos disponibles

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -6,9 +6,22 @@ use App\Models\Evento;
 use App\Models\Cuota;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 
 class EventoController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $eventos = Evento::with('cuotas')
+            ->where('estado', 'pendiente')
+            ->get();
+
+        return response()->json($eventos);
+    }
+
+    public function show(int $id): JsonResponse
+    {
+        $evento = Evento::with('cuotas')->findOrFail($id);
+
+        return response()->json($evento);
+    }
 }
