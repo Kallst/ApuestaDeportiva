fb598dc00cc4c701791be3994e9e05a0f5fe74d5
Kallst <santiago.castrom@autonoma.edu.co>
2026-03-15 12:26:05 -0500

    Se configuró el seeder CuotaSeeder para contar con cuotas de prueba en el sistema

--- database/seeders/CuotaSeeder.php
+++ database/seeders/CuotaSeeder.php
@@ -2,17 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Cuota;
 
 class CuotaSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Cuota::create([
+            'evento_id' => 1,
+            'tipo_apuesta' => 'local',
+            'cuota' => 1.8
+        ]);
+
+        Cuota::create([
+            'evento_id' => 1,
+            'tipo_apuesta' => 'empate',
+            'cuota' => 3.2
+        ]);
+
+        Cuota::create([
+            'evento_id' => 1,
+            'tipo_apuesta' => 'visitante',
+            'cuota' => 2.5
+        ]);
     }
 }
