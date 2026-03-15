5aab5989630ca021b30f267c8c265d8828762ace
Kallst <santiago.castrom@autonoma.edu.co>
2026-03-15 11:26:17 -0500

    Se configuró el UserController con los respectivos métodos para consultar la información del usuario junto con sus datos y saldo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Apuesta;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    
+    public function perfil(): JsonResponse
+    {
+        $user = Auth::user();
+
+        return response()->json($user);
+    }
+
+    public function saldo(): JsonResponse
+    {
+        $user = Auth::user();
+
+        return response()->json([
+            'saldo' => $user->saldo
+        ]);
+    }
 }
