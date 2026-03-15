c7b7a89803e38a507a02f7cf049fedf393365bf5
Kallst <santiago.castrom@autonoma.edu.co>
2026-03-15 11:30:13 -0500

    Se configuró el CheckEventoAbiertoMiddleware que verificará que haya un evento existente antes de registrar una apuesta

--- app/Http/Middleware/CheckEventoAbiertoMiddleware.php
+++ app/Http/Middleware/CheckEventoAbiertoMiddleware.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Evento;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Models\Evento;
 
 class CheckEventoAbiertoMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  Closure(Request): (Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
+        $evento = Evento::find($request->evento_id);
+
+        if (!$evento || $evento->estado !== 'pendiente') {
+            return response()->json([
+                'message' => 'El evento no está disponible para apostar'
+            ], 400);
+        }
+
         return $next($request);
     }
 }
