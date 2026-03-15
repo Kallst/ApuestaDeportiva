b4e613534c21fcfc3afd5d1deb07c6516d68aae8
Kallst <santiago.castrom@autonoma.edu.co>
2026-03-15 12:05:40 -0500

    Se configuró el archivo app.php de la carpeta bootsrap para que las rutas y alias de los middleware queden definidos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,24 +1,34 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\RoleMiddleware;
 use App\Http\Middleware\OtpMiddleware;
 use App\Http\Middleware\CheckSaldoMiddleware;
 use App\Http\Middleware\CheckEventoAbiertoMiddleware;
 
-
 return Application::configure(basePath: dirname(__DIR__))
+
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+
+    ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->alias([
+            'role' => RoleMiddleware::class,
+            'otp' => OtpMiddleware::class,
+            'saldo' => CheckSaldoMiddleware::class,
+            'evento.abierto' => CheckEventoAbiertoMiddleware::class,
+        ]);
+
+    })
+
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+
+    ->create();
