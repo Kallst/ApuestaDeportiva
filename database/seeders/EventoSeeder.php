7d2a1831edcbd0ca09f20e97494c1bd586b8422c
Kallst <santiago.castrom@autonoma.edu.co>
2026-03-15 12:24:52 -0500

    Se configuró el seeder UserSeeder para contar con eventos de prueba en el sistema

--- database/seeders/EventoSeeder.php
+++ database/seeders/EventoSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Evento;
+
 class EventoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Evento::create([
+            'deporte' => 'Fútbol',
+            'equipo_local' => 'Real Madrid',
+            'equipo_visitante' => 'Barcelona',
+            'fecha' => '2026-06-10',
+            'estado' => 'pendiente'
+        ]);
+
+        Evento::create([
+            'deporte' => 'Fútbol',
+            'equipo_local' => 'Manchester City',
+            'equipo_visitante' => 'Liverpool',
+            'fecha' => '2026-06-11',
+            'estado' => 'pendiente'
+        ]);
     }
 }
